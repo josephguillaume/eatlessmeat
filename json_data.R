c5d1b3c5bdc7019eb2c17d6473be033230943552
Joseph Guillaume <josephguillaume@gmail.com>
2014-12-24 08:19:49 +0200

    Added country ISO codes to data.js

--- json_data.R
+++ json_data.R
@@ -1,6 +1,7 @@
 ## Export data to js to be loaded in browser
 
 library(eatlessmeat)
+rm(countries)
 library(RJSONIO)
 
 sink("inst/www/js/data.js")
@@ -33,5 +34,39 @@ cat("var greenfootprintlgmatrix =",toJSON(greenfootprintlgmatrix),";\n")
 colnames(bluefootprintlgmatrix)=NULL
 rownames(bluefootprintlgmatrix)=NULL
 cat("var bluefootprintlgmatrix =",toJSON(bluefootprintlgmatrix),";\n")
+
+
+iso3166=read.csv("../GeoLite2-Country-Locations-en.csv",stringsAsFactors=FALSE)
+corrections=c(
+  "Bolivia (Plurinational State of)"="Bolivia",
+  "Brunei Darussalam"="Brunei",
+  "Côte d'Ivoire" ="Ivory Coast",
+  "Democratic People's Republic of Korea"="North Korea",
+  "Democratic Republic of the Congo"="Republic of the Congo",
+  "Iran (Islamic Republic of)"="Iran",
+  "Jordan"="Hashemite Kingdom of Jordan",
+  "Lao People's Democratic Republic"="Laos",
+  "Lithuania"="Republic of Lithuania", 
+  "Myanmar"="Myanmar [Burma]", 
+  "Netherlands Antilles"="CuraÃ§ao", #also Bonaire and Sint Maarten 
+  "Occupied Palestinian Territory"="Palestine", 
+  "Russian Federation"="Russia", 
+  #"Sao Tome and Principe"=""SÃ£o TomÃ© and PrÃ?ncipe""
+  "Syrian Arab Republic"="Syria", 
+  "The former Yugoslav Republic of Macedonia"="Macedonia", 
+  "Timor-Leste"="East Timor",
+  "United Republic of Tanzania"="Tanzania", 
+  "United States of America"="United States", 
+  "Venezuela (Bolivarian Republic of)"="Venezuela", 
+  "Viet Nam"="Vietnam"
+)
+countries[match(names(corrections),countries)]<-corrections
+iso3166$country_name[131]="Sao Tome and Principe"
+w=match(countries,iso3166$country_name)
+
+#countries[is.na(w)]
+stopifnot(all(!is.na(w)))
+
+cat("var iso3166 =",toJSON(iso3166$country_iso_code[w]),";\n")
 
 sink()
